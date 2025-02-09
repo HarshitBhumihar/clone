8f111745f60fa539c9314b535fd3c0412b28e2ee
HarshitBhumihar <157011504+HarshitBhumihar@users.noreply.github.com>
2025-02-09 18:36:51 +0530

    package.json updated

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,18 +1,18 @@
 import User from "../models/user.model.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 export const getUserProfileAndRepos = async (req, res) => {
 	const { username } = req.params;
 
 	
-	try {
+	try {		
 		// 60 requests per hour, 5000 requests per hour for authenticated requests
 		// https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28
-		const userRes = await fetch(`https://api.github.com/users/${username}`, {
+	const userRes = await fetch(`https://api.github.com/users/${username}`, {
 			headers: {
 				authorization: `token ${process.env.GITHUB_API_KEY}`,
 			},
 		});
-
 		const userProfile = await userRes.json();
 
 		const repoRes = await fetch(userProfile.repos_url, {
